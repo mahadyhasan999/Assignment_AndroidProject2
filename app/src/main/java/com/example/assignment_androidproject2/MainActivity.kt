4bc10ec8354c503684678ee8aeb646b8c14668ab
mahadyhasan999 <mahady.cse.city@gmail.com>
2023-12-20 02:10:39 +0600

    FrontEND done

--- app/src/main/java/com/example/assignment_androidproject2/MainActivity.kt
+++ app/src/main/java/com/example/assignment_androidproject2/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.assignment_androidproject2
 
+//1. Create Two activity Main and Home.
+//2. In Main you need to design a signup page and send the data by intent. (profile image, username, name, email, age, address will be taken from user)
+//3. In Home you need show data by intent which is send by Main.
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
